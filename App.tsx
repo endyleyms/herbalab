6ab6e5151353a2bdac2be4ccb2334b2fe7901491
wendy-medina <endyleyms@gmail.com>
2022-05-11 18:52:40 -0500

    created login page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import Login from './Pages/Login';
 
-
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <TailwindProvider utilities={utilities}>
-     <Login/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
--- Pages/Login.tsx
+++ Pages/Login.tsx
@@ -1,8 +1,49 @@
+import React from "react";
+import { SafeAreaView, TextInput, StyleSheet, View, Button, Text } from "react-native";
 import Hello from '../Components/MyComponent';
+import {useTailwind} from 'tailwind-rn';
 
 
 export default function Login() {
+  const tailwind = useTailwind();
+  const [email, onChangeEmail] = React.useState("");
+  const [password, onChangePassword] = React.useState("");
+  
   return (
-     <Hello/>
+    <SafeAreaView>
+      <Hello />
+      <form >
+        <View>
+          <Text>Email</Text>
+          <TextInput
+          style={tailwind('w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4')}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="Your email"
+        />
+        </View>
+        <View>
+        <Text>Password</Text>
+         <TextInput
+         style={tailwind('w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4')}
+          onChangeText={onChangePassword}
+          value={password}
+          secureTextEntry={true}
+          placeholder="Your password"
+          /> 
+        </View>
+        <Button 
+        title="Login"
+        // onPress={}
+        />  
+      </form>
+      <Text>Forgot the password?</Text>
+      <View>
+        <Text>Do'nt have an account? Sing up</Text>
+      </View>
+          
+      
+    </SafeAreaView>
   );
-}
+};
+
