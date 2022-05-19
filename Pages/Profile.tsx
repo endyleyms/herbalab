ad9b63adf66360fc90c42acf62297124a61443a2
wendy-medina <endyleyms@gmail.com>
2022-05-19 12:51:06 -0500

    pintando image

--- Pages/Profile.tsx
+++ Pages/Profile.tsx
@@ -52,7 +52,7 @@ const Profile = () => {
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: photoURL }} style={{ width: 200, height: 200, borderRadius: 100 }} />
+      <Image source={{ uri: image }} style={{ width: 200, height: 200, borderRadius: 100 }} />
       <Pressable style={styles.buton}>
         <Text style={styles.text} onPress={uploadFile} >Upload image</Text>
       </Pressable>
