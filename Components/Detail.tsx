169253d6fe07b193b269c1e9ebbc4c3be3df9cec
wendy-medina <endyleyms@gmail.com>
2022-05-23 18:39:42 -0500

    agregando estilos a detail

--- Components/Detail.tsx
+++ Components/Detail.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { collection, doc, getDoc, getDocs} from 'firebase/firestore';
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React, { useEffect } from 'react'
+import { doc, getDoc} from 'firebase/firestore';
 import firebaseModule from '../database/firebase'
 
 const Detail = ({route}) => {
@@ -16,19 +16,49 @@ const Detail = ({route}) => {
         console.log("No such document!");
       }
   }
+  const image= group.image
   
   useEffect(()=> {
     getFamilia()
   }, [])
   return (
-    <View>
-      <Text>{group.familia}</Text>
-      <Text>{group.genero}</Text>
-      <Text>{group.especie}</Text>
+    <View style={styles.container}>
+        <Text style={styles.familia}>Familia: {group.familia}</Text>
+        <Image 
+        style={{ width: '20%', height: '20%', alignSelf: 'center' }}
+        source={{uri: image}} />
+        <Text style={styles.text}>Género: {group.genero}</Text>
+        <Text style={styles.text}>Especie: {group.especie}</Text>
+        <Text style={styles.text}>{group.description}</Text>
     </View>
   )
 }
 
 export default Detail
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        position: 'relative',
+        left: 30,
+        top: 40,
+        height: '80%',
+        width: '80%',
+        backgroundColor: 'white',
+        marginVertical: 5,
+        borderRadius: 5,
+        paddingBottom: 5
+    },
+    familia:{
+        backgroundColor: '#D2D9D2',
+        width: '100%',
+        height: '10%',
+        padding: 10,
+    },
+    text:{
+        width: '90%',
+        left:20
+    }
+})
